f5edb468940286c27a8fbcad262d0236a56e2024
yash agarwal <agarwalyash9120076911@gmail.com>
2023-08-13 18:39:10 +0530

    Added work expirence column achievements etc

--- script.js
+++ script.js
@@ -1,52 +1,7 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    checkInputs();
-});
-
-function checkInputs() {
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if (emailValue === '') {
-        setErrorFor(email, 'Email cannot be blank');
-    } else {
-        setSuccessFor(email);
-
-    }
-    if (passwordValue === '') {
-        setErrorFor(password, 'Password cannot be blank');
-    } else {
-        setSuccessFor(password);
-    }
-
-    if (password2Value === '') {
-        setErrorFor(password2, 'Confirm Password cannot be blank');
-    } else if (passwordValue !== password2Value) {
-        setErrorFor(password2, 'Confirm Password do not match');
-    } else {
-        setSuccessFor(password2);
-        window.location = 'PersonalPortfolio.html';
-    }
-};
-
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    formControl.className = 'form-control error';
-    small.innerText = message;
-}
-
-function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-}
-
-function isEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
+document.addEventListener('DOMContentLoaded', function() {
+    const elementsToFadeIn = document.querySelectorAll('.fade-in');
+    elementsToFadeIn.forEach(function(element) {
+      element.classList.add('show');
+    });
+  });
+console.log("Hello")
